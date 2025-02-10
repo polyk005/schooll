8b2620d792e044bf25ef28c43af9f5df5f918293
Midiy <polyk6859@gmail.com>
2025-02-10 19:22:37 +0300

    update struct

--- cmd/main.go
+++ cmd/main.go
@@ -4,15 +4,14 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/Glack134/websocket/pkg/handler"
-	"github.com/Glack134/websocket/pkg/repository"
-	"github.com/Glack134/websocket/pkg/service"
 	"github.com/joho/godotenv"
+	OnlineSchooleProject "github.com/polyk005/school"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
 
 func main() {
-	logrus.Farmatter(new(logrus.JSONFormatter))
+	logrus.SetFormatter(new(logrus.JSONFormatter))
 
 	if err := initConfig(); err != nil {
 		logrus.Fatalf("error initializing configs: %s", err.Error())
@@ -22,27 +21,11 @@ func main() {
 		logrus.Fatalf("error loading env variables: %s", err.Error())
 	}
 
-	db, err := repository.NewPostgresDB(repository.Config{
-		Host:     viper.GetString("db.host"),
-		Port:     viper.GetString("db.port"),
-		Username: viper.GetString("db.username"),
-		DBName:   viper.GetString("db.dbname"),
-		SSLMode:  viper.GetString("db.sslmode"),
-		Password: viper.GetString("db.password"),
-	})
-	if err != nil {
-		logrus.Fatalf("failed to initalize db: %s", err.Error())
-	}
-
 	fmt.Println("Start server...")
 
-	repos := repository.NewRepository(db)
-	services := service.NewService(repos)
-	handlers := handler.NewHandler(services)
+	srv := new(OnlineSchooleProject.Server)
 
-	srv := new(web_socket.Server)
-
-	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port")); err != nil {
 		log.Fatalf("error occured while running http server %s", err.Error())
 	}
 }
